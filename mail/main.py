8ffacad2bc957fc5ee20672edfbea2f0376b8361
connectinggrandparents <connectinggrandparents@gmail.com>
2025-10-31 15:18:45 +0100

    Update main.py

--- mail/main.py
+++ mail/main.py
@@ -5,23 +5,27 @@ import random
 import os
 from mail import send_mail
 
-from spam import send_verification
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/comment',methods=['POST'])
+
+@app.route('/comment', methods=['POST'])
 def comment():
   req = request.get_json()
-  return {'response':send_mail(
-    req.get('name','unknown'),
-    req.get('surnames','unknown'),
-    req.get('text','unknown'),
-    'connectinggrandparents@gmail.com'
-  )}
+  return {
+      'response':
+      send_mail(req.get('name', 'unknown'), req.get('surnames', 'unknown'),
+                req.get('text', 'unknown'), 'connectinggrandparents@gmail.com')
+  }
+
+
+@app.route('/')
+def index():
+  return jsonify({'response': 'ok'})
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render la define automáticamente
-    app.run(host="0.0.0.0", port=port)         # ← host correcto
+  port = int(os.environ.get("PORT", 8080))  # Render la define automáticamente
+  app.run(host="0.0.0.0", port=port)  # ← host correcto
